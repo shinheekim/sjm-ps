e545bd983f4a2eb169a6ee1100e966f4ea649c85
ksp <become1213@naver.com>
2025-07-12 20:39:35 +0900

    [BOJ] 2749 피보나치 수 - 골드 2

--- kaseongpil/src/boj_2749/Main.java
+++ kaseongpil/src/boj_2749/Main.java
@@ -1,8 +1,6 @@
 package boj_2749;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
     static int pisanoPeriod(int mod) {
