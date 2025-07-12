336281a157958bc67a74f06165cb2fe3865fa325
ksp <become1213@naver.com>
2025-07-12 20:44:14 +0900

    [BOJ] 15657 N과 M (8) - 실버 3

---
+++ kaseongpil/src/boj_15657/Main.java
@@ -0,0 +1,53 @@
+//package boj_15657;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static int[] sequence;
+    static int[] answer;
+    static StringBuilder sb = new StringBuilder();
+
+    static void bfs(int depth, int cur) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++)
+                sb.append(answer[i]).append(" ");
+            sb.append("\n");
+            return;
+        }
+        for (int i = 0; i < N; i++) {
+            if (sequence[cur] <= sequence[i]) {
+                answer[depth] = sequence[i];
+                bfs(depth + 1, i);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        var br = new BufferedReader(new InputStreamReader(System.in));
+
+        var st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        answer = new int[M];
+        sequence = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++)
+            sequence[i] = Integer.parseInt(st.nextToken());
+
+        Arrays.sort(sequence);
+
+        bfs(0, 0);
+
+        System.out.print(sb);
+
+        br.close();
+    }
+}
